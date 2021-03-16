63fd9ea4d583d1d75e4e9330060a8c874ace71e1
thugtech97 <juhde.1997@gmail.com>
2021-03-16 10:24:54 +0800

    committed

--- php/php_general_functions.php
+++ php/php_general_functions.php
@@ -20,11 +20,12 @@ function get_complete_name($name){
 	return (array_key_exists($name,$fnwd)) ? $fnwd[$name] : $name;
 }
 
-function get_account_code($category,$index){
+function get_account_code($type, $category,$index){
+	$acc_code = "";
 	$account_codes = array(
 		"ICT Equipments"=>array("10405030","10605030"),
 		"Communication Equipments"=>array("10405070","10605070"),
-		"Drugs and Medicines"=>array("10402990",""),
+		"Drugs and Medicines"=>array("10401010-02",""),
 		"Furniture"=>array("10406010","10607010"),
 		"Housekeeping Supplies"=>array("",""),
 		"ICT Supplies"=>array("10405030","10605030"),
@@ -34,11 +35,54 @@ function get_account_code($category,$index){
 		"Office Equipment"=>array("10405020","10605020"),
 		"Office Supplies"=>array("10404010",""),
 		"Other Supplies"=>array("10404990","10604990"),
-		"Property and Equipment for Distribution"=>array("10402090","10602090"),
 		"Software Application"=>array("10405030","10605030"),
 		"Various Supplies"=>array("10404990","10604990")
 	);
-	return (array_key_exists($category, $account_codes)) ? $account_codes[$category][$index] : "";
+	$ptr_codes = array(
+		"Property and Equipment for Distribution"=>array("10402090","10602090"),
+		"Drugs and Medicines"=>array("10402030",""),
+		"Textbooks and Instructional Materials for Distribution"=>array("10402070",""),
+		"Other Supplies and Materials for Distribution"=>array("10402990","10602990")
+	);
+
+	if($type != "PTR"){
+		$acc_code = (array_key_exists($category, $account_codes)) ? $account_codes[$category][$index] : "";
+	}else{
+		switch($category){
+			case "Drugs and Medicines":
+				$acc_code = $ptr_codes[$category][0];
+				break;
+			case "Medical Supplies":
+				$acc_code = $ptr_codes["Other Supplies and Materials for Distribution"][0];
+				break;
+			case "Office Supplies":
+				$acc_code = $ptr_codes["Other Supplies and Materials for Distribution"][0];
+				break;
+			case "Various Supplies":
+				$acc_code = $ptr_codes["Other Supplies and Materials for Distribution"][0];
+				break;
+			case "Other Supplies":
+				$acc_code = $ptr_codes["Other Supplies and Materials for Distribution"][0];
+				break;
+			case "ICT Supplies":
+				$acc_code = $ptr_codes["Other Supplies and Materials for Distribution"][0];
+				break;
+			case "Housekeeping Supplies":
+				$acc_code = $ptr_codes["Other Supplies and Materials for Distribution"][0];
+				break;
+			case "Library":
+				$acc_code = $ptr_codes["Textbooks and Instructional Materials for Distribution"][0];
+				break;
+			default:
+				$acc_code = $ptr_codes["Property and Equipment for Distribution"][0];
+				break;
+		}
+	}
+	return $acc_code;
+}
+
+function check_pn_exist(){
+
 }
 
 ?>
--- php/php_sc.php
+++ php/php_sc.php
@@ -149,7 +149,7 @@ function get_rsmi_details(){
 		$unit = $row["unit"];
 		$quantity = $row["quantity"];
 		$category = $row["category"];
-		$account_code = get_account_code($category, 0);
+		$account_code = get_account_code("RIS", $category, 0);
 		$requested_by = $row["requested_by"];
 		$unit_cost = $row["unit_cost"];
 		mysqli_query($conn, "INSERT INTO tbl_rsmi(date_released,control_no,item,unit,quantity,recipients,unit_cost,account_code,rcc) VALUES('$date_released','$ris_no','$description','$unit','$quantity','$requested_by','$unit_cost','$account_code','$rcc')");
@@ -189,7 +189,7 @@ function get_ppe_details(){
 		$cost = $row["cost"];
 		$total = $row["total"];
 		$category = $row["category"];
-		$account_code = ($cost < 15000) ? get_account_code($category, 0) : get_account_code($category, 1);
+		$account_code = ($cost < 15000) ? get_account_code("ICS", $category, 0) : get_account_code("ICS" ,$category, 1);
 		$received_by = $row["received_by"];
 		$remarks = $row["remarks"];
 		mysqli_query($conn, "INSERT INTO tbl_ppe(tbl_ppe.date,particular,par_ptr_reference,qty,unit,unit_cost,total_cost,type,received_by,remarks,account_code) VALUES('$date_released','$item','$reference_no','$quantity','$unit','$cost','$total','ics','$received_by','$remarks','$account_code')");
@@ -205,7 +205,7 @@ function get_ppe_details(){
 		$cost = $row["cost"];
 		$total = $row["total"];
 		$category = $row["category"];
-		$account_code = ($cost < 15000) ? get_account_code($category, 0) : get_account_code($category, 1);
+		$account_code = ($cost < 15000) ? get_account_code("PAR", $category, 0) : get_account_code("PAR", $category, 1);
 		$received_by = $row["received_by"];
 		$remarks = $row["remarks"];
 		mysqli_query($conn, "INSERT INTO tbl_ppe(tbl_ppe.date,particular,par_ptr_reference,qty,unit,unit_cost,total_cost,type,received_by,remarks,account_code) VALUES('$date_released','$item','$reference_no','$quantity','$unit','$cost','$total','par','$received_by','$remarks','$account_code')");
@@ -235,7 +235,7 @@ function get_ppe_details(){
 		$cost = $row["cost"];
 		$total = $row["total"];
 		$category = $row["category"];
-		$account_code = ($cost < 15000) ? get_account_code($category, 0) : get_account_code($category, 1);
+		$account_code = ($cost < 15000) ? get_account_code("PTR", $category, 0) : get_account_code("PTR", $category, 1);
 		$to = $row["to"];
 		$remarks = $row["remarks"];
 		mysqli_query($conn, "INSERT INTO tbl_ppe(tbl_ppe.date,particular,par_ptr_reference,qty,unit,unit_cost,total_cost,type,received_by,remarks,account_code) VALUES('$date_released','$item','$reference_no','$quantity','$unit','$cost','$total','ptr','$to','$remarks','$account_code')");
