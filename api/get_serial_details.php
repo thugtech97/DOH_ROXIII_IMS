b60b2ea5f30fefb95dbdee96ec1e96b9aaaf6822
thugtech97 <juhde.1997@gmail.com>
2023-03-23 11:31:24 +0800

    cors api allowed

--- api/get_serial_details.php
+++ api/get_serial_details.php
@@ -3,7 +3,7 @@
 require "../php/php_conn.php";
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+//header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 
 function api_process($serial, $property_no){
 	global $conn;
