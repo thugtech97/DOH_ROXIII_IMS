1687c152da97225ab45963e67182b8c334be3f29
thugtech97 <juhde.1997@gmail.com>
2024-08-29 14:42:24 +0800

    remove pass for aws windows server

--- php/php_conn.php
+++ php/php_conn.php
@@ -4,7 +4,7 @@ $set = 0;
 
 $server = ($set == 0) ? "localhost" : "192.168.1.15";
 $username = ($set == 0) ? "root" : "root";
-$password = ($set == 0) ? "password" : "";
+$password = ($set == 0) ? "" : "";
 
 $conn = mysqli_connect($server, $username, $password, "supply");
 $connhr = mysqli_connect($server, $username, $password, "hr");
