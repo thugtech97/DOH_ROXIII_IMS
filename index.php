92d808b4e5b7351c30c896ce9b144d718ce00474
thugtech97 <juhde.1997@gmail.com>
2024-09-01 14:22:21 +0800

    dynamic navigation

--- index.php
+++ index.php
@@ -40,75 +40,7 @@ if(!isset($_SESSION["username"])){
 </head>
 <body>
     <div id="wrapper">
-        <nav class="navbar-default navbar-static-side" role="navigation">
-            <div class="sidebar-collapse">
-                <ul class="nav metismenu" id="side-menu">
-                    <li class="nav-header">
-                        <div class="dropdown profile-element">
-                                <center><img alt="image" class="rounded-circle" src="../archives/img/<?php echo $_SESSION["company_logo"]; ?>" height="50" width="50"/>
-                                <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                                    <span class="block m-t-xs font-bold"><?php echo $_SESSION["username"]; ?></span>
-                                    <span class="text-muted text-xs block"><?php echo $_SESSION["role"]; ?><b class="caret"></b></span>
-                                </a>
-                            <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                <li><a class="dropdown-item" href="">Profile</a></li>
-                                <li><a class="dropdown-item" href="">Contacts</a></li>
-                                <li><a class="dropdown-item" href="">Mailbox</a></li>
-                                <li class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="php/php_logout.php">Logout</a></li>
-                            </ul>
-                            </center>
-                        </div>
-                        <div class="logo-element">
-                            IMS+
-                        </div>
-                    </li>
-                    <li class="active">
-                        <a href="index.php"><i class="fa fa-th-large"></i> <span class="nav-label"><?php echo $_SESSION["link0"]; ?></span></a>
-                    </li>
-                    <li>
-                        <a href="po.php"><i class="fa fa-list-alt"></i> <span class="nav-label">Purchase Orders</span></a>
-                    </li>
-                    <li>
-                        <a href=""><i class="fa fa-clipboard"></i> <span class="nav-label"><?php echo $_SESSION["link1"]; ?></span><span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li><a href="supply_records/iar.php"><i class="fa fa-clipboard"></i> IAR</a></li>
-                            <li><a href="supply_records/ics.php"><i class="fa fa-clipboard"></i> ICS</a></li>
-                            <li><a href="supply_records/par.php"><i class="fa fa-clipboard"></i> PAR</a></li>
-                            <li><a href="supply_records/ris.php"><i class="fa fa-clipboard"></i> RIS</a></li>
-                            <li><a href="supply_records/ptr.php"><i class="fa fa-clipboard"></i> PTR</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href=""><i class="fa fa-table"></i> <span class="nav-label"><?php echo $_SESSION["link2"]; ?></span><span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li><a href="reference_tables/area.php"><i class="fa fa-area-chart"></i> Area</a></li>
-                            <li><a href="reference_tables/rcc.php"><i class="fa fa-code-fork"></i> RCC</a></li>
-                            <li><a href="reference_tables/category.php"><i class="fa fa-tag"></i> Category</a></li>
-                            <li><a href="reference_tables/item.php"><i class="fa fa-object-group"></i> Item</a></li>
-                            <li><a href="reference_tables/unit.php"><i class="fa fa-balance-scale"></i> Unit</a></li>
-                            <li><a href="reference_tables/supplier.php"><i class="fa fa-users"></i> Supplier</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="stockcard.php"><i class="fa fa-bars"></i> <span class="nav-label">Stock Card</span></a>
-                    </li>
-                    <li>
-                        <a href="sai.php"><i class="fa fa-search"></i> <span class="nav-label">For SAI</span></a>
-                    </li>
-                    <li>
-                        <a href="archives.php"><i class="fa fa-archive"></i> <span class="nav-label">Archive</span></a>
-                    </li>
-                    <li>
-                        <a href="https://drive.google.com/file/d/1H6DzrxhfjMdY0wh9NiCpfuq5_hdbgc5H/view?usp=sharing" target="_blank"><i class="fa fa-book"></i> <span class="nav-label">User Manual</span></a>
-                    </li>
-                    <li>
-                        <a href="php/php_logout.php"><i class="fa fa-power-off"></i> <span class="nav-label">Logout</span></a>
-                    </li>
-                </ul>
-
-            </div>
-        </nav>
+        <?php include("assets/navbar.php"); ?>
 
         <div id="page-wrapper" class="gray-bg dashbard-1" style="color: black;">
         <div class="row border-bottom">
